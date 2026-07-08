7eeede67b491b35e234c562e51e33a1b2494e707
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-07-08 17:44:21 -0300

    csv file transferred to postgresql database

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+CSV_PATH=os.getenv("CSV_PATH")
--- db_ingestion.py
+++ db_ingestion.py
@@ -1,12 +1,24 @@
 from sqlalchemy import create_engine, insert,  text
-from config import SQLALCHEMY_DATABASE_URL
+from config import SQLALCHEMY_DATABASE_URL, CSV_PATH
+import pandas as pd
 import time
 
 time.sleep(5)
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
-conn=engine.connect()
 
-query=text("INSERT INTO identity (_name, surname) VALUES ('Carlos', 'Daniel'), ('Maria', 'Eduarda');")
-conn.execute(query)
+df = pd.read_csv(CSV_PATH)
+df.head(0).to_sql("responses", engine, if_exists="replace", index=False)
+
+conn=engine.raw_connection()
+cur=conn.cursor()
+
+with open(CSV_PATH, "r", encoding="utf-8") as csv_file:
+    cur.copy_expert(
+        "COPY responses FROM STDIN WITH CSV HEADER",
+        csv_file
+    )
+
 conn.commit()
+cur.close()
+conn.close()
